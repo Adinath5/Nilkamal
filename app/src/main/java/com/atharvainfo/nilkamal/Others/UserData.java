64983d9da3a9dea37bbdc5c524aa4a1cc06ebafa
adinath5 <atharvainfosolutions@gmail.com>
2019-11-04 19:29:37 +0530

    411

---
+++ app/src/main/java/com/atharvainfo/nilkamal/Others/UserData.java
@@ -0,0 +1,63 @@
+package com.atharvainfo.nilkamal.Others;
+
+public class UserData {
+    private String username;
+    private String empname;
+    private String phone;
+    private String usertp;
+    private String address;
+    private String emailid;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public String getEmpname() {
+        return empname;
+    }
+
+    public void setEmpname(String empname) {
+        this.empname = empname;
+    }
+
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public String getUsertp() {
+        return usertp;
+    }
+
+    public void setUsertp(String usertp) {
+        this.usertp = usertp;
+    }
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmailid(){
+        return emailid;
+    }
+    public void setEmailid(String emailid){
+        this.emailid = emailid;
+        }
+
+
+}
